74c7c501f5dc5fe639358cc2e03279174ae56bf3
SHADOW JOKER <77511106+mehebubhasanshimul@users.noreply.github.com>
2026-06-14 01:06:28 +0600

    Add game categories to loadMemberCenter response
    Updated the response structure to include categories of games with titles and lists.

--- api/loadMemberCenter.js
+++ api/loadMemberCenter.js
@@ -2,15 +2,41 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
 
-    // গেমের তালিকা এবং আপনার দেওয়া থাম্বনেইল ইমেজ লিঙ্ক এখানে আপডেট করা হয়েছে
+    // স্ক্রিনশটের ক্যাটাগরি ডাটা আর্কিটেকচার
     res.status(200).json({
-        games: [
-            { id: "1", name: "ক্রেজি স্লটস (Slots)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
-            { id: "2", name: "লাইভ রুলেট (Casino)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
-            { id: "3", name: "এভিয়েটর ক্র্যাশ (Crash)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
-            { id: "4", name: "ফিশিং কিং (Fishing)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
-            { id: "5", name: "স্পোর্টস বুক (Sports)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
-            { id: "6", name: "মেগা লটারি (Lottery)", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" }
+        categories: [
+            {
+                title: "🔥 Hot Games",
+                list: [
+                    { name: "Super Ace", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Crazy Worm", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "FlyX Slot", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" }
+                ]
+            },
+            {
+                title: "⚽ Sports Betting",
+                list: [
+                    { name: "9Wickets Sports", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Lucky Sports", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "BTI Sports", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" }
+                ]
+            },
+            {
+                title: "🃏 Live Casino",
+                list: [
+                    { name: "Evolution Gaming", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Pragmatic Play", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Micro Gaming", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" }
+                ]
+            },
+            {
+                title: "🐠 Fishing & Fish",
+                list: [
+                    { name: "Happy Fishing", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Fortune King", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" },
+                    { name: "Mega Fishing", image: "https://i.ibb.co.com/TB0dCbY3/file-00000000b038720794f86c010a439e4c.png" }
+                ]
+            }
         ]
     });
 }
